da83c0577e9f503f90e49a1c03538489cbe245c6
ColleenCallonego <colleencallonego@gmail.com>
2021-05-14 22:31:05 +0200

    Version Consultation du projet - Menu

--- src/main/java/fr/ul/miage/Consultation/Main.java
+++ src/main/java/fr/ul/miage/Consultation/Main.java
@@ -12,9 +12,11 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 
 public class Main {
+    public static String log;
     public static void main (String[] args){
-        System.out.println("Bienvenue dans le logiciel de consultation de document");
-        System.out.println("Entrez votre login pour vous connectez :");
+        String n = System.getProperty("line.separator");
+        System.out.println("Bienvenue dans le logiciel de consultation de document" + n
+                            + "Entrez votre login pour vous connectez :");
         Scanner scanner = new Scanner(System.in);
         String login = scanner.nextLine();
         Boolean ok = connect(login);
@@ -24,6 +26,8 @@ public class Main {
             ok = connect(login);
         }
         System.out.println("Connection réussie !");
+        log = login;
+        ecranPrincipal();
     }
 
     public static Boolean connect(String login){
@@ -32,5 +36,63 @@ public class Main {
         MongoCollection<Document> collectionUtilisateur = database.getCollection("utilisateur");
         Long present = collectionUtilisateur.countDocuments(eq("login", login));
         return (present != 0);
+    }
+
+    public static void ecranPrincipal(){
+        Integer rep = menu();
+        while (rep != 0){
+            switch (rep){
+                case 1:
+                    rechercheTitre();
+                    break;
+                case 2:
+                    rechercheMotsCles();
+                    break;
+                case 3:
+                    rechercheThematique();
+                    break;
+                case 4:
+                    listeMieuxNotees();
+                    break;
+                case 5:
+                    listeCommenteeRecemment();
+                    break;
+            }
+        }
+    }
+
+    public static Integer menu(){
+        String n = System.getProperty("line.separator");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bienvenue " + log + n
+                + "Que souhaitez vous faire ? (Entrez le numéro de l'action voulue)" + n
+                + "1. Une recherche par titre" + n
+                + "2. Une recherche par mots-clés" + n
+                + "3. Une recherche par thématique" + n
+                + "4. Afficher une liste des publications les mieux 10 (10 oeuvres)" + n
+                + "5. Afficher une liste des publications commentées récemment (10 oeuvres)" + n
+                + "0. Se déconnecter");
+        Integer choix = scan.nextInt();
+        return choix;
+    }
+
+    public static void rechercheTitre() {
+
+    }
+
+    public static void rechercheMotsCles() {
+
+    }
+
+    public static void rechercheThematique() {
+
+    }
+
+    public static void listeMieuxNotees() {
+
+    }
+
+    public static void listeCommenteeRecemment() {
+
     }
 }
