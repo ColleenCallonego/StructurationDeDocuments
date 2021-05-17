53544f86f14f7c557ac6e78b26586c2fcc9abe8b
ColleenCallonego <colleencallonego@gmail.com>
2021-05-17 16:57:45 +0200

    Version Consultation du projet - ajout des méthodes pour ajouter un commentaire et mettre à jour la note moyenne dans oeuvre

--- src/main/java/fr/ul/miage/Consultation/Main.java
+++ src/main/java/fr/ul/miage/Consultation/Main.java
@@ -1,11 +1,16 @@
 package fr.ul.miage.Consultation;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
 
 import static com.mongodb.client.model.Filters.and;
@@ -14,7 +19,7 @@ import static com.mongodb.client.model.Filters.eq;
 public class Main {
     public static String log;
     public static void main (String[] args){
-        String n = System.getProperty("line.separator");
+        /*String n = System.getProperty("line.separator");
         System.out.println("Bienvenue dans le logiciel de consultation de document" + n
                             + "Entrez votre login pour vous connectez :");
         Scanner scanner = new Scanner(System.in);
@@ -27,7 +32,14 @@ public class Main {
         }
         System.out.println("Connection réussie !");
         log = login;
-        ecranPrincipal();
+        ecranPrincipal();*/
+        log = "McdonnellC";
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("projet");
+        MongoCollection<Document> collectionUtilisateur = database.getCollection("commentaire");
+        String s = "60a26f49f0808914f9244524";
+        ObjectId o = new ObjectId(s);
+        addCommentaire(collectionUtilisateur, "C'est très bien", "Les oubliés d'encre", "2011-03-15" , 8);
     }
 
     public static Boolean connect(String login){
@@ -94,5 +106,41 @@ public class Main {
 
     public static void listeCommenteeRecemment() {
 
+    }
+
+    public static void addCommentaire (MongoCollection<Document> collection, String commentaire, String titre, String datePublicationOeuvre, Integer note){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date datePublicationO = new Date();
+        try {
+            datePublicationO = new SimpleDateFormat("yyyy-MM-dd").parse(datePublicationOeuvre);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Document doc = new Document();
+        doc.put("titre", titre);
+        doc.put("datePublicationOeuvre", formatter.format(datePublicationO));
+        Document d = new Document();
+        d.put("auteur", log);
+        d.put("oeuvre", doc);
+        d.put("contenu", commentaire);
+        d.put("note", note);
+        collection.insertOne(d);
+        updateNoteMoyenneOeuvre(collection, titre, datePublicationOeuvre);
+    }
+
+    public static void updateNoteMoyenneOeuvre(MongoCollection<Document> collectionCommentaire, String titre, String datePublicationOeuvre){
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("projet");
+        MongoCollection<Document> collectionOeuvre = database.getCollection("oeuvre");
+        Document moyenne = collectionCommentaire.aggregate(
+                Arrays.asList(
+                        Aggregates.match(Filters.eq("oeuvre.titre", titre)),
+                        Aggregates.match(Filters.eq("oeuvre.datePublicationOeuvre", datePublicationOeuvre)),
+                        Aggregates.group("$oeuvre.titre", Accumulators.avg("noteMoyenne", "$note"))
+                )
+        ).first();
+        Document d = new Document();
+        d.put("NoteMoyenne", moyenne.get("noteMoyenne"));
+        collectionOeuvre.updateOne(and(eq("titre", titre), eq("datePublication", datePublicationOeuvre)), new Document("$set", d));
     }
 }
